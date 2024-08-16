7ad428e70dabb092f7d0c90596002cef3c87cd71
Utkarsh Kumar Singh <104830759+UtkarshKS37@users.noreply.github.com>
2024-08-17 02:41:05 +0530

    Add files via upload

---
+++ initialize/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int value1 = 10, value2, value3;
+        value2 = 101;
+        value3 = value1*4;
+        System.out.println(value1+value2+value3);
+    }
+}
