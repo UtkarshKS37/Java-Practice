2ebf6d83c25924da43a7f91a791eda3068f47708
Utkarsh Kumar Singh <104830759+UtkarshKS37@users.noreply.github.com>
2024-08-18 03:22:23 +0530

    Add files via upload

---
+++ mod-op/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int mod = 21%2;
+        System.out.println(mod);
+        int x = 7;
+        System.out.println(++x); //prefix operation
+        System.out.println(x++); //postfix increment operation
+        System.out.println(x);
+        System.out.println(--x); //prefix decrement operation
+        System.out.println(x);
+    }
+}
