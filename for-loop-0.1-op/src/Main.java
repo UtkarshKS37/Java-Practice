6e0d7a6120ec6715ca819c94efe8a483cdb1e843
Utkarsh Kumar Singh <104830759+UtkarshKS37@users.noreply.github.com>
2024-08-23 03:33:54 +0530

    Add files via upload

---
+++ for-loop-0.1-op/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(20);
+        System.out.println(21);
+        System.out.print("22\n23\n24");
+    }
+}
