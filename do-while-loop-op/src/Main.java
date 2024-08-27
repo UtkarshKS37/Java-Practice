a5ab939c8fe645b079d6ff8f51dfc2d6f004f78e
Utkarsh Kumar Singh <104830759+UtkarshKS37@users.noreply.github.com>
2024-08-28 01:29:10 +0530

    Add files via upload

---
+++ do-while-loop-op/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 2;
+        do {
+            System.out.println("Hello world" + i);
+            i++;
+        } while (i > 8);
+    }
+}
