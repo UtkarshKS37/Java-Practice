c4e11ddd573cb4515aefe30363b1885e89000065
Utkarsh Kumar Singh <104830759+UtkarshKS37@users.noreply.github.com>
2024-09-04 18:41:25 +0530

    Add files via upload

---
+++ nested-loop-op/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 9; j > 4; j--) {
+                System.out.println("i value:" + i + " j value:" + j);
+            }
+            System.out.println("--------------------------iteration gap for i");
+        }
+    }
+}
